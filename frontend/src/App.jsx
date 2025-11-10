ed478d3c9f39c24c4433e630ac560b71d62ec74b
def-himani <hm3686@nyu.edu>
2025-11-10 16:33:03 -0500

    removed demo from app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import LandingPage from './pages/LandingPage'
-import QueryDemo from './pages/QueryDemo'
 import axios from 'axios'
 
 const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5004'
