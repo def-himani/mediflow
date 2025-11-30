6e6f515675a4796d4a0447611e0e69e63da0c0ce
IvanYeung0610 <ivanyeung0610@gmail.com>
2025-11-30 11:34:16 -0500

    Fixed time zone error for date of birth

--- frontend/src/pages/PatientProfile.jsx
+++ frontend/src/pages/PatientProfile.jsx
@@ -161,6 +161,7 @@ export default function PatientProfile() {
             const formatDateForSQL = (dateString) => {
                 if (!dateString) return null;
                 const date = new Date(dateString);
+                date.setDate(date.getDate() + 1); // Adjust for timezone issues
                 const year = date.getFullYear();
                 const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-index
                 const day = String(date.getDate()).padStart(2, '0');
